39310af47ea69dad36dae2e7f5bcf0a06d2586cd
Moises <ve11621@innovaccion.mx>
2022-10-28 11:31:58 -0500

    No logro hacer funcionar esta chingadera

--- OAuth2MicroService/auth/src/main/java/com/oauthserver/auth/AuthServerConfiguration/AuthConfiguration.java
+++ OAuth2MicroService/auth/src/main/java/com/oauthserver/auth/AuthServerConfiguration/AuthConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -28,6 +29,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -38,59 +44,47 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
-@Configuration
-@EnableWebSecurity
-public class AuthConfiguration{
-
-	@Bean
-	public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
-		return http
-				.csrf(csrf->csrf.disable())
-				.authorizeRequests(
-					auth->
-					auth.anyRequest().authenticated()
-				)
-				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-				.httpBasic(Customizer.withDefaults())
-				.build();
-	}
-
-	@Bean
-	public InMemoryUserDetailsManager user(){
-		return new InMemoryUserDetailsManager(User.withUsername("n").password("{noop}1").authorities("read").build());
-	}
-}
-/*
 @Configuration(proxyBeanMethods = false)
-public class AuthConfiguration{
+@EnableWebSecurity
+public class AuthConfiguration {
 
-	@Bean
+	/*
+	 * @Bean
+	 * public SecurityFilterChain securityFilterChain (HttpSecurity http) throws
+	 * Exception{
+	 * return http
+	 * .csrf(csrf->csrf.disable())
+	 * .authorizeRequests(
+	 * auth->
+	 * auth.anyRequest().authenticated()
+	 * )
+	 * .sessionManagement(session->session.sessionCreationPolicy(
+	 * SessionCreationPolicy.STATELESS))
+	 * .httpBasic(Customizer.withDefaults())
+	 * .build();
+	 * }
+	 */
+
 	@Order(Ordered.HIGHEST_PRECEDENCE)
 	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 		return http.formLogin(Customizer.withDefaults()).build();
 	}
 
-
-
-    @Bean
-    public UserDetailsService users() {    
-        UserDetails user = User.withUsername("u")
-                .password(passwordEncoder().encode("1"))
-                .roles("USER")
-                .build();
-        
-        return new InMemoryUserDetailsManager(user);
-        
-    }
-
-
-	
-
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	public InMemoryUserDetailsManager user() {
+		/*
+		 * The createDelegatingPasswordEncoder saves passwords in bcrypt by default
+		 * and it appears you can save many passwords in many different encryption
+		 * algorithms
+		 * by using prefixes
+		 */
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return new InMemoryUserDetailsManager(
+				User.withUsername("n")
+						.password(encoder.encode("1"))
+						.authorities("read")
+						.build());
 	}
 
 	@Bean
@@ -100,10 +94,12 @@ public class AuthConfiguration{
 				.clientSecret("secret")
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
 				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
 				.redirectUri("http://127.0.0.1:8080/authorized")
+				.redirectUri("https://spring.io/auth")
 				.scope(OidcScopes.OPENID)
-				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				// .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
 				.build();
 
 		return new InMemoryRegisteredClientRepository(registeredClient);
@@ -122,6 +118,7 @@ public class AuthConfiguration{
 		return new ImmutableJWKSet<>(jwkSet);
 	}
 
+	@Bean
 	private static KeyPair generateRsaKey() {
 		KeyPair keyPair;
 		try {
@@ -138,4 +135,87 @@ public class AuthConfiguration{
 	public ProviderSettings providerSettings() {
 		return ProviderSettings.builder().build();
 	}
-} */
+}
+/*
+ * @Configuration(proxyBeanMethods = false)
+ * public class AuthConfiguration{
+ * 
+ * @Bean
+ * 
+ * @Order(Ordered.HIGHEST_PRECEDENCE)
+ * public SecurityFilterChain
+ * authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+ * OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+ * return http.formLogin(Customizer.withDefaults()).build();
+ * }
+ * 
+ * 
+ * 
+ * @Bean
+ * public UserDetailsService users() {
+ * UserDetails user = User.withUsername("u")
+ * .password(passwordEncoder().encode("1"))
+ * .roles("USER")
+ * .build();
+ * 
+ * return new InMemoryUserDetailsManager(user);
+ * 
+ * }
+ * 
+ * 
+ * 
+ * 
+ * @Bean
+ * public PasswordEncoder passwordEncoder() {
+ * return NoOpPasswordEncoder.getInstance();
+ * }
+ * 
+ * @Bean
+ * public RegisteredClientRepository registeredClientRepository() {
+ * RegisteredClient registeredClient =
+ * RegisteredClient.withId(UUID.randomUUID().toString())
+ * .clientId("client")
+ * .clientSecret("secret")
+ * .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+ * .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+ * .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
+ * .redirectUri("http://127.0.0.1:8080/authorized")
+ * .scope(OidcScopes.OPENID)
+ * .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).
+ * build())
+ * .build();
+ * 
+ * return new InMemoryRegisteredClientRepository(registeredClient);
+ * }
+ * 
+ * @Bean
+ * public JWKSource<SecurityContext> jwkSource() {
+ * KeyPair keyPair = generateRsaKey();
+ * RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+ * RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+ * RSAKey rsaKey = new RSAKey.Builder(publicKey)
+ * .privateKey(privateKey)
+ * .keyID(UUID.randomUUID().toString())
+ * .build();
+ * JWKSet jwkSet = new JWKSet(rsaKey);
+ * return new ImmutableJWKSet<>(jwkSet);
+ * }
+ * 
+ * private static KeyPair generateRsaKey() {
+ * KeyPair keyPair;
+ * try {
+ * KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+ * keyPairGenerator.initialize(2048);
+ * keyPair = keyPairGenerator.generateKeyPair();
+ * } catch (Exception ex) {
+ * throw new IllegalStateException(ex);
+ * }
+ * return keyPair;
+ * }
+ * 
+ * @Bean
+ * public ProviderSettings providerSettings() {
+ * return ProviderSettings.builder().build();
+ * }
+ * }
+ */
